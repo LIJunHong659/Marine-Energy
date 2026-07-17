9769e34b56f083ccb9f564e588cc8d477927e4af
LIJunHong659 <1346469839@qq.com>
2026-07-17 16:31:28 +0800

    增加配置文件修改参数

--- submodules/src/bluehub_submodules/__init__.py
+++ submodules/src/bluehub_submodules/__init__.py
@@ -10,7 +10,13 @@ from .compute_load import ComputeLoadParams, ComputeLoadResult, evaluate_compute
 from .hydrogen_output import HydrogenParams, HydrogenResult, evaluate_hydrogen_output
 from .marine_load import MarineLoadParams, MarineLoadResult, evaluate_marine_load
 from .objectives import ObjectiveBreakdown, calculate_objective_breakdown
-from .parameters import ModelParameters, default_parameters
+from .parameters import (
+    ModelParameters,
+    default_parameters,
+    load_parameters_from_file,
+    load_parameters_from_yaml,
+    parameters_from_mapping,
+)
 from .power_export import PowerExportParams, PowerExportResult, evaluate_power_export
 from .scenario import generate_typical_day, simple_greedy_dispatch
 
@@ -35,7 +41,9 @@ __all__ = [
     "evaluate_marine_load",
     "evaluate_power_export",
     "generate_typical_day",
+    "load_parameters_from_file",
+    "load_parameters_from_yaml",
     "simple_greedy_dispatch",
+    "parameters_from_mapping",
     "summarize_results",
 ]
-
